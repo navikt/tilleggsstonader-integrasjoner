a9438086a92129d283d418f1b4d572f4a41a29d9
Johan Blomgren <blommish@users.noreply.github.com>
2023-09-14 14:56:09 +0200

    ktlint

--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/oppgave/OppgaveController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/oppgave/OppgaveController.kt
@@ -12,7 +12,6 @@ import no.nav.tilleggsstonader.kontrakter.oppgave.Oppgave
 import no.nav.tilleggsstonader.kontrakter.oppgave.OppgaveResponse
 import no.nav.tilleggsstonader.kontrakter.oppgave.OpprettOppgaveRequest
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
