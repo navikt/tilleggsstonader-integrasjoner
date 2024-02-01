d31227844757f6cd8080af8fd089d346555e37e6
Johan Blomgren <blommish@users.noreply.github.com>
2024-02-01 16:49:08 +0100

    Lagt til dokumenttype for internt vedtak tilsyn barn

--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/metadata/BarnetilsynVedtaksbrevMetadata.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/metadata/BarnetilsynVedtaksbrevMetadata.kt
@@ -13,7 +13,7 @@ object BarnetilsynVedtaksbrevMetadata : Dokumentmetadata {
     override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
     override val fagsakSystem: Fagsystem = Fagsystem.TILLEGGSSTONADER
     override val tema: Tema = Tema.TSO
-    override val behandlingstema: Behandlingstema = Behandlingstema.Barnetilsyn
+    override val behandlingstema: Behandlingstema = Behandlingstema.TilsynBarn
     override val kanal: String? = null
     override val dokumenttype: Dokumenttype = Dokumenttype.BARNETILSYN_VEDTAKSBREV
     override val tittel: String? = null
