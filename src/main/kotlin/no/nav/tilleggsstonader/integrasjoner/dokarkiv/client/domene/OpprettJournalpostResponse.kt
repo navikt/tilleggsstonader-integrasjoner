aad5aa440b4aa1f66e56eea33ee383a3220c242f
Johan Blomgren <blommish@users.noreply.github.com>
2024-02-28 11:08:04 +0100

    Fjerner kode som ikke burde vært committet

--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/client/domene/OpprettJournalpostResponse.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/client/domene/OpprettJournalpostResponse.kt
@@ -1,9 +1,6 @@
 package no.nav.tilleggsstonader.integrasjoner.dokarkiv.client.domene
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.tilleggsstonader.kontrakter.dokarkiv.DokumentInfo
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 class OpprettJournalpostResponse(
     val journalpostId: String? = null,
@@ -11,20 +8,3 @@ class OpprettJournalpostResponse(
     val journalpostferdigstilt: Boolean? = false,
     val dokumenter: List<DokumentInfo>? = null,
 )
-
-fun main() {
-    Jackson2ObjectMapperBuilder.json().build<ObjectMapper>().readValue<OpprettJournalpostResponse>(
-        """{
-  "dokumenter": [
-    {
-      "brevkode": "NAV 34-00.08",
-      "dokumentInfoId": "123",
-      "tittel": "Søknad om kontanstøtte"
-    }
-  ],
-  "journalpostId": "12345678",
-  "journalpostferdigstilt": false,
-  "journalstatus": "MIDLERTIDIG"
-}""",
-    )
-}
