fb63772da121d9e5032088d06aacc347d0a9c909
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-15 14:58:41 +0100

    Henter og mapper kun de der man har rett på stønaden

--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/arena/ArenaService.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/arena/ArenaService.kt
@@ -1,6 +1,7 @@
 package no.nav.tilleggsstonader.integrasjoner.arena
 
 import no.nav.tilleggsstonader.kontrakter.aktivitet.AktivitetDto
+import no.nav.tilleggsstonader.kontrakter.felles.ObjectMapperProvider.objectMapper
 import no.nav.tilleggsstonader.libs.log.SecureLogger.secureLogger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -16,14 +17,16 @@ class ArenaService(
     fun hentAktiviteter(ident: String, fom: LocalDate, tom: LocalDate?): List<AktivitetDto> {
         val aktiviteter = arenaClient.hentAktiviteter(ident, fom, tom)
 
-        return aktiviteter.mapNotNull {
-            try {
-                AktivitetDtoMapper.map(it)
-            } catch (e: Exception) {
-                logger.error("Feilet mapping av aktivitet, se secure logs for mer info")
-                secureLogger.error("Feilet mapping av aktivitet, $it")
-                null
+        return aktiviteter
+            .filter { it.erStoenadsberettigetAktivitet == true }
+            .mapNotNull {
+                try {
+                    AktivitetDtoMapper.map(it)
+                } catch (e: Exception) {
+                    logger.error("Feilet mapping av aktivitet, se secure logs for mer info")
+                    secureLogger.error("Feilet mapping av aktivitet, ${objectMapper.writeValueAsString(it)}", e)
+                    null
+                }
             }
-        }
     }
 }
