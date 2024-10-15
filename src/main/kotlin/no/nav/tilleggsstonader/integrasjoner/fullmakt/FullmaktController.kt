6e2b3e934cffffb6bf2c19f7b58c32299b538abd
Runar Skagestad <runar.skagestad@nav.no>
2024-10-15 15:14:39 +0200

    Endepunkt for fullmektige manglet @RequestBody-annotasjon

--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/fullmakt/FullmaktController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/fullmakt/FullmaktController.kt
@@ -4,6 +4,7 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.tilleggsstonader.kontrakter.felles.IdentRequest
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,7 +16,7 @@ class FullmaktController(
     val fullmaktClient: PdlFullmaktClient,
 ) {
     @PostMapping("fullmektige")
-    fun hentFullmektige(fullmaktsgiver: IdentRequest): List<FullmektigDto> {
+    fun hentFullmektige(@RequestBody fullmaktsgiver: IdentRequest): List<FullmektigDto> {
         return fullmaktClient.hentFullmektige(fullmaktsgiver.ident)
     }
 }
