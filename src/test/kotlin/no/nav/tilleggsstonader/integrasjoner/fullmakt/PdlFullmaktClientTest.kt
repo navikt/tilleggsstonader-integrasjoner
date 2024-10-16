662150300cd2ce35217e725442ec22d16c893d1f
Runar Skagestad <runar.skagestad@nav.no>
2024-10-16 15:45:52 +0200

    Forenkler test av base64 encoding

--- src/test/kotlin/no/nav/tilleggsstonader/integrasjoner/fullmakt/PdlFullmaktClientTest.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/integrasjoner/fullmakt/PdlFullmaktClientTest.kt
@@ -1,17 +1,16 @@
 import no.nav.tilleggsstonader.integrasjoner.fullmakt.FullmaktIdentPdlRequest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.util.Base64
 
 class PdlFullmaktClientTest {
 
     @Test
     fun `ident skal være encodet som base64`() {
         val ident = "12345678910"
+        val identSomBase64 = "MTIzNDU2Nzg5MTA="
 
         val request = FullmaktIdentPdlRequest.create(ident)
 
-        val forventetIdent = Base64.getEncoder().encodeToString(ident.toByteArray())
-        assertEquals(forventetIdent, request.ident)
+        assertEquals(identSomBase64, request.ident)
     }
 }
