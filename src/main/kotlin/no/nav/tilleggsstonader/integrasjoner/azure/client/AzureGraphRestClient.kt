e84b843edc95842f9bec1c9acea7f956c7f4daf4
Johan Blomgren <blommish@users.noreply.github.com>
2024-10-29 10:49:09 +0100

    Erstatter familie sin AbstractRestClient med vår restClient

--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/azure/client/AzureGraphRestClient.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/azure/client/AzureGraphRestClient.kt
@@ -1,53 +1,45 @@
 package no.nav.tilleggsstonader.integrasjoner.azure.client
 
-import no.nav.familie.http.client.AbstractRestClient
 import no.nav.tilleggsstonader.integrasjoner.azure.domene.AzureAdBruker
 import no.nav.tilleggsstonader.integrasjoner.azure.domene.AzureAdBrukere
+import no.nav.tilleggsstonader.libs.http.client.AbstractRestClient
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestOperations
+import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
 
-/**  TODO: Bruker familie sin AbstractRestClient og RestOperations for å få at kallet går igjennom til Azure
- * se favro for forklaring og hva problemet er https://favro.com/organization/98c34fb974ce445eac854de0/4d617346d79341c7fbd9a40a?card=NAV-21832
- */
-
 @Service
 class AzureGraphRestClient(
-    @Qualifier("azure") restTemplate: RestOperations,
+    @Qualifier("azure") restTemplate: RestTemplate,
     @Value("\${clients.azure-graph.uri}") private val aadGraphURI: URI,
-) :
-    AbstractRestClient(restTemplate, "azureGraph-ts-integrasjoner") {
-
-    fun saksbehandlerUri(id: String): URI =
-        UriComponentsBuilder.fromUri(aadGraphURI)
-            .pathSegment(USERS, id)
-            .queryParam("\$select", FELTER)
-            .build()
-            .toUri()
-
-    fun saksbehandlersøkUri(navIdent: String): URI =
-        UriComponentsBuilder.fromUri(aadGraphURI)
-            .pathSegment(USERS)
-            .queryParam("\$search", "\"onPremisesSamAccountName:{navIdent}\"") // Denne linjen blir det feilencoding på mot azureGraph
-            .queryParam("\$select", FELTER)
-            .buildAndExpand(navIdent)
-            .toUri()
+) : AbstractRestClient(restTemplate) {
 
     fun finnSaksbehandler(navIdent: String): AzureAdBrukere {
+        val uri = UriComponentsBuilder.fromUri(aadGraphURI)
+            .pathSegment(USERS)
+            .queryParam("\$search", "\"onPremisesSamAccountName:{navIdent}\"")
+            .queryParam("\$select", FELTER)
+            .encode()
+            .toUriString()
         return getForEntity(
-            saksbehandlersøkUri(navIdent),
+            uri,
             HttpHeaders().apply {
                 add("ConsistencyLevel", "eventual")
             },
+            uriVariables = mapOf("navIdent" to navIdent)
         )
     }
 
     fun hentSaksbehandler(id: String): AzureAdBruker {
-        return getForEntity(saksbehandlerUri(id))
+        val uri = UriComponentsBuilder.fromUri(aadGraphURI)
+            .pathSegment(USERS, "{id}")
+            .queryParam("\$select", FELTER)
+            .encode()
+            .toUriString()
+        return getForEntity(uri, uriVariables = mapOf("id" to id))
     }
 
     companion object {
