d8edf445e1dbbcdab2c412b5e7791e19b3eb2709
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-14 11:37:39 +0100

    ktlint - Renamer klasser då de kun inneholder en type nå

--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/metadata/KlageInterntVedtak.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/metadata/KlageInterntVedtak.kt
@@ -3,18 +3,16 @@ package no.nav.tilleggsstonader.integrasjoner.dokarkiv.metadata
 import no.nav.tilleggsstonader.integrasjoner.dokarkiv.client.domene.JournalpostType
 import no.nav.tilleggsstonader.kontrakter.dokarkiv.Dokumenttype
 import no.nav.tilleggsstonader.kontrakter.felles.Behandlingstema
-import no.nav.tilleggsstonader.kontrakter.felles.Fagsystem
 import no.nav.tilleggsstonader.kontrakter.felles.Tema
-import org.springframework.stereotype.Component
 
-sealed class KlageVedtak(
+sealed class KlageInterntVedtak(
     final override val tema: Tema,
     final override val behandlingstema: Behandlingstema,
     override val dokumenttype: Dokumenttype,
 ) : Dokumentmetadata {
-    override val journalpostType: JournalpostType = JournalpostType.UTGAAENDE
+    override val journalpostType: JournalpostType = JournalpostType.NOTAT
     override val kanal: String? = null
     override val tittel: String? = null
-    override val brevkode: String = "KLAGE_VEDTAKSBREV_${behandlingstema.name.uppercase()}"
+    override val brevkode: String = "KLAGE_INTERNT_VEDTAK_${behandlingstema.name.uppercase()}"
     override val dokumentKategori: Dokumentkategori = Dokumentkategori.VB
 }
