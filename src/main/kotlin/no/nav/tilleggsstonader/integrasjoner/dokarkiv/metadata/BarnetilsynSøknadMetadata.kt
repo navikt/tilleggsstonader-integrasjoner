fb2e0f331396c216329d6193a17c5284cf180fd1
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-14 11:02:45 +0100

    Fellesmetadata for søknad metadata

--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/metadata/BarnetilsynSøknadMetadata.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/metadata/BarnetilsynSøknadMetadata.kt
@@ -1,21 +1,17 @@
 package no.nav.tilleggsstonader.integrasjoner.dokarkiv.metadata
 
-import no.nav.tilleggsstonader.integrasjoner.dokarkiv.client.domene.JournalpostType
 import no.nav.tilleggsstonader.kontrakter.dokarkiv.Dokumenttype
 import no.nav.tilleggsstonader.kontrakter.felles.Behandlingstema
-import no.nav.tilleggsstonader.kontrakter.felles.Fagsystem
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
 import no.nav.tilleggsstonader.kontrakter.felles.Tema
+import no.nav.tilleggsstonader.kontrakter.sak.DokumentBrevkode
 import org.springframework.stereotype.Component
 
 @Component
-object BarnetilsynSøknadMetadata : Dokumentmetadata {
-    override val journalpostType: JournalpostType = JournalpostType.INNGAAENDE
-    override val fagsakSystem: Fagsystem = Fagsystem.TILLEGGSSTONADER
-    override val tema: Tema = Tema.TSO
-    override val behandlingstema: Behandlingstema = Behandlingstema.TilsynBarn
-    override val kanal: String = "NAV_NO"
-    override val dokumenttype: Dokumenttype = Dokumenttype.BARNETILSYN_SØKNAD
-    override val tittel: String = "Søknad om støtte til pass av barn"
-    override val brevkode: String = "NAV 11-12.15"
-    override val dokumentKategori: Dokumentkategori = Dokumentkategori.SOK
-}
+data object BarnetilsynSøknadMetadata : SøknadMetadata(
+    tema = Tema.TSO,
+    behandlingstema = Behandlingstema.TilsynBarn,
+    dokumenttype = Dokumenttype.BARNETILSYN_SØKNAD,
+    tittel = "Søknad om ${Stønadstype.BARNETILSYN.visningsnavn}",
+    brevkode = DokumentBrevkode.BARNETILSYN.verdi,
+)
--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/metadata/LæremidlerSøknadMetadata.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/dokarkiv/metadata/LæremidlerSøknadMetadata.kt
@@ -1,22 +1,17 @@
 package no.nav.tilleggsstonader.integrasjoner.dokarkiv.metadata
 
-import no.nav.tilleggsstonader.integrasjoner.dokarkiv.client.domene.JournalpostType
 import no.nav.tilleggsstonader.kontrakter.dokarkiv.Dokumenttype
 import no.nav.tilleggsstonader.kontrakter.felles.Behandlingstema
-import no.nav.tilleggsstonader.kontrakter.felles.Fagsystem
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
 import no.nav.tilleggsstonader.kontrakter.felles.Tema
 import no.nav.tilleggsstonader.kontrakter.sak.DokumentBrevkode
 import org.springframework.stereotype.Component
 
 @Component
-data object LæremidlerSøknadMetadata : Dokumentmetadata {
-    override val journalpostType: JournalpostType = JournalpostType.INNGAAENDE
-    override val fagsakSystem: Fagsystem = Fagsystem.TILLEGGSSTONADER
-    override val tema: Tema = Tema.TSO
-    override val behandlingstema: Behandlingstema = Behandlingstema.Læremidler
-    override val kanal: String = "NAV_NO"
-    override val dokumenttype: Dokumenttype = Dokumenttype.LÆREMIDLER_SØKNAD
-    override val tittel: String = "Søknad om støtte til læremidler"
-    override val brevkode: String = DokumentBrevkode.LÆREMIDLER.verdi
-    override val dokumentKategori: Dokumentkategori = Dokumentkategori.SOK
-}
+data object LæremidlerSøknadMetadata : SøknadMetadata(
+    tema = Tema.TSO,
+    behandlingstema = Behandlingstema.Læremidler,
+    dokumenttype = Dokumenttype.LÆREMIDLER_SØKNAD,
+    tittel = "Søknad om ${Stønadstype.LÆREMIDLER.visningsnavn}",
+    brevkode = DokumentBrevkode.LÆREMIDLER.verdi,
+)
