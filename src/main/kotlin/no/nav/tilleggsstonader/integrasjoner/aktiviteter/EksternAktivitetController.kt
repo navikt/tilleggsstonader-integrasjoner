6ad405c4cec1ba2c421ff6af1ec0a49cd86a581d
Nibedita.Pahari <nibedita.pahari@nav.no>
2025-11-05 14:42:47 +0100

    oppdatert label på aktivitet dersom periode er null eller tom er null fix

--- src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/aktiviteter/EksternAktivitetController.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/integrasjoner/aktiviteter/EksternAktivitetController.kt
@@ -50,7 +50,7 @@ enum class AktivitetSøknadType {
 fun AktivitetArenaDto.tilDto(): AktivitetSøknadDto {
     val dato =
         fom?.let {
-            "${it.norskDatoTekstligMåned()} - ${ tom?.let { tom?.norskDatoTekstligMåned()?:"pågoende" }}"
+            "${it.norskDatoTekstligMåned()} - ${tom?.norskDatoTekstligMåned() ?: "pågående"}"
         } ?: ""
     return AktivitetSøknadDto(
         id = id,
